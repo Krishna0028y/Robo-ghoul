bd2641deea26fe365e49d1416e80a08923775692
Krishna0028y <75119804+Krishna0028y@users.noreply.github.com>
2022-03-03 19:06:17 +0530

    Add files via upload

---
+++ robo.py
@@ -0,0 +1,46 @@
+print("yo! I am Ghoul What's Your Name Kid?")
+name = input("")
+print("I see, That's a nice name you got" , name, "..Well Aside that how old are you?")
+age=int(input(""))
+
+if (age>18):
+    print("POWER OF YOUTH LESS GOO..Wanna play a game? I am bored..(YES/NO)(REMEMBER THE CAPS)")
+
+elif (age>13):
+    print("So you're a teen..interesting..Wanna play a game? i am bored..(YES/NO)(REMEMBER THE CAPS)")
+
+else :
+    print("keed lmao! ja jake padai kar")
+
+
+ans = input("")
+
+if (ans=="NO"):
+    print("ah You're no fun!! well, Maybe next time, till then BYE!!")
+if (ans=="YES"):
+    print("okay then less start with a trivia shall we?(YES/NO)(REMEMBER THE CAPS)")
+
+ansg = input("")
+
+if (ansg=="NO"):
+    print("sorry dude we ain't got any other game")
+if (ans=="YES"):
+    print("Who invented Computer?")
+
+anst = input("")
+
+if (anst =="Charles Babbage..also don't type A in the next input"):
+    print("YOU WON! ",name)
+    
+else :
+    print("YOU LOOSE THE ANSWER IS...Charles Babbage..also don't type A in the next input")
+
+ansr = input("")
+
+if (ansr =="A"):
+    print("don't follow this link...https://youtu.be/dQw4w9WgXcQ")
+
+
+   
+
+
